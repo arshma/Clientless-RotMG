1cb486f066988e5c724b148144e51313d2ebe9d1
Arsh Mangat <43280802+arshma@users.noreply.github.com>
2019-03-04 23:55:37 -0800

    Add files via upload

--- src/listeners/Proxy.java
+++ src/listeners/Proxy.java
@@ -1,12 +1,8 @@
 package listeners;
 
-import gamedata.GameData;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import net.Client;
 import net.packets.Packet;
 import net.packets.Packet.*;
@@ -14,28 +10,51 @@ import net.packets.client.*;
 import net.packets.dataobjects.Entity;
 import net.packets.dataobjects.Location;
 import net.packets.dataobjects.LocationRecord;
+import net.packets.dataobjects.Status;
 import net.packets.server.*;
 
 //This class maintains lists of all the listerners that need to be informed upon a certain event.
-//This a centeral location where a client retreives a list of all the listeners.
 public class Proxy {
-    private ArrayList<PacketListener> serverPacketReceived = null;
-    private Map<PacketListener, ArrayList<PacketType>> packetHooks = null;
+    private ArrayList<PacketListener> serverPacketReceived;
+    private Map<PacketType, PacketListener> packetHooks;
     
     public Proxy() {
-        this.packetHooks = new HashMap<PacketListener, ArrayList<PacketType>>(20);
         this.serverPacketReceived = new ArrayList<PacketListener>(5);
-        //Add default listeners
-        this.hookPacket(PacketType.MAPINFO, new PacketListener() {
+        this.packetHooks = new HashMap<PacketType, PacketListener>(40);
+        this.setDefaultListeners();
+    }
+    
+    
+    //Registers listeners/callbacks specified packet types. A single listener/callback
+    //can be associated with multiple packet types.
+    public void hookPacket(PacketType pType, PacketListener listener) {
+        this.packetHooks.put(pType, listener);
+    }
+    
+    //Notifys registered listeners/callbacks when a packet is received from the server.
+    public void fireServerPacket(Client client, Packet packet) {
+        if(packet == null) {
+            System.out.println("ERROR::Proxy.java: Packet is null");
+        }
+        PacketListener listener = this.packetHooks.get(packet.type());
+        if(listener != null)
+            listener.onPacketReceived(client, packet);
+        else
+            System.out.println("ERROR:Proxy.java: Unable to find listener for packet type [" + packet.type() + "]");
+    }   
+    
+    private void setDefaultListeners() {
+        this.packetHooks.put(PacketType.MAPINFO, new PacketListener() {
             @Override
             public void onPacketReceived(Client client, Packet packet) {
-                LoadPacket loadPacket = (LoadPacket)Packet.create(PacketType.LOAD);
+                LoadPacket loadPacket = new LoadPacket();
                 loadPacket.characterId = 85;
                 loadPacket.isFromArena = false;
                 client.sendQueue.add(loadPacket);
             }
         });
-        this.hookPacket(PacketType.CREATESUCCESS, new PacketListener() {
+        
+        this.packetHooks.put(PacketType.CREATESUCCESS, new PacketListener() {
             @Override
             public void onPacketReceived(Client client, Packet packet) {
                 CreateSuccessPacket csp = (CreateSuccessPacket)packet;
@@ -43,87 +62,80 @@ public class Proxy {
                 client.myCharId = csp.charId;
             }
         });
-        this.hookPacket(PacketType.UPDATE, new PacketListener() {
+        
+        this.packetHooks.put(PacketType.UPDATE, new PacketListener() {
             @Override
             public void onPacketReceived(Client client, Packet packet) {
-                UpdateAckPacket uap = (UpdateAckPacket)Packet.create(PacketType.UPDATEACK);
+                UpdateAckPacket uap = new UpdateAckPacket();
+                client.sendQueue.add(uap);
                 
                 UpdatePacket up = (UpdatePacket)packet;
-                for(Entity e : up.newObjs) {
-                    if(e.status.objectId == client.myObjectId) {
+                for(Entity ent : up.newObjs) {
+                    if(ent.status.objectId == client.myObjectId) {
                         try {
-                            client.position = (Location)e.status.position.clone();
-                        } catch (CloneNotSupportedException ex) {
-                            Logger.getLogger(Proxy.class.getName()).log(Level.SEVERE, null, ex);
+                            client.position = (Location)ent.status.position.clone();
+                            break;
+                        } catch (CloneNotSupportedException e) {
+                            System.out.println("ERROR::Proxy.java: Unable to set char position");
+                            e.printStackTrace();
                         }
                     }
-                }
-                client.sendQueue.add(uap);                
+                }                                
             }
         });
-        this.hookPacket(PacketType.NEWTICK, new PacketListener() {
+        
+        this.packetHooks.put(PacketType.NEWTICK, new PacketListener() {
             @Override
             public void onPacketReceived(Client client, Packet packet) {
-                try {
-                    MovePacket movePacket = (MovePacket)Packet.create(PacketType.MOVE);
-                    
-                    NewTickPacket ntp = (NewTickPacket)packet;
-                    client.tickId = ntp.tickId;
-                    client.previousTickTime = ntp.tickTime;
-                    
-                    movePacket.tickId = client.tickId;
-                    movePacket.time = ((int)System.currentTimeMillis()) - client.clientStartTime;
-                    movePacket.newPosition = (Location)client.position.clone();
-                    movePacket.records = new ArrayList<LocationRecord>();
-                    client.sendQueue.add(movePacket);
-                } catch (CloneNotSupportedException ex) {
-                    Logger.getLogger(Proxy.class.getName()).log(Level.SEVERE, null, ex);
+                NewTickPacket ntp = (NewTickPacket)packet;                
+                client.lastTickTime = ntp.tickTime;
+                client.lastTickId = ntp.tickId;
+                client.currentTickTime = client.getTime();
+                
+                //Get positino of char if not already attained.
+                if(client.position.x == 0.0 && client.position.y == 0.0) {
+                    try {
+                        for(Status s : ntp.statuses) {
+                            if(client.myObjectId == s.objectId) {
+                                client.position = (Location)s.position.clone();
+                                break;
+                            }
+                        }
+                    } catch(Exception e) {
+                        System.out.println("ERROR::Proxy.java: Unable to set location.");
+                        e.printStackTrace();
+                        //exit.
+                    }
                 }
+                
+                //reply with move packet
+                MovePacket movePacket = new MovePacket();
+                movePacket.tickId = ntp.tickId;
+                movePacket.time = client.getTime();
+                movePacket.newPosition = client.position;
+                movePacket.records = new ArrayList<LocationRecord>();
+                client.sendQueue.add(movePacket);                
             }
         });
-        this.hookPacket(PacketType.PING, new PacketListener() {
+        
+        this.packetHooks.put(PacketType.PING, new PacketListener() {
             @Override
             public void onPacketReceived(Client client, Packet packet) {
                 PingPacket pingPacket = (PingPacket)packet;
-                PongPacket pongPacket = (PongPacket)Packet.create(PacketType.PONG);
                 
-                client.pingSerial = pingPacket.serial;
-                
-                pongPacket.serial = client.pingSerial;
-                pongPacket.time = ((int)System.currentTimeMillis()) - client.clientStartTime;
+                PongPacket pongPacket = new PongPacket();
+                pongPacket.serial = pingPacket.serial;
+                pongPacket.time = client.getTime();
                 client.sendQueue.add(pongPacket);
             }
         });
-    }
-    
-    public void start() {}
-    public void stop() {}
-    
-    //Registers listeners/callbacks specified packet types. A single listener/callback
-    //can be associated with multiple packet types.
-    public void hookPacket(PacketType pType, PacketListener listener) {
-        //check for invalid packet types.
-        if(GameData.packets.byName(pType.toString()).packetType == PacketType.UNKNOWN) {
-            throw new java.lang.IllegalArgumentException();
-        } else if(this.packetHooks.containsKey(listener)) {
-            this.packetHooks.get(listener).add(pType);
-        } else {
-            ArrayList<PacketType> list = new ArrayList<PacketType>();
-            list.add(pType);
-            this.packetHooks.put(listener, list);
-        }
-    }
-    
-    //Notifys registered listeners/callbacks when a packet is received from the server.
-    public void fireServerPacket(Client client, Packet packet) {
-        //Notify listeners/callbacks when a server packet is received(regardless of type). --needs implementation
         
-        //Notify hooked listeners/callbacks.
-        Set<Map.Entry<PacketListener, ArrayList<PacketType>>> entrySet = this.packetHooks.entrySet();
-        for(Map.Entry<PacketListener, ArrayList<PacketType>> pair : entrySet) {
-            if(pair.getValue().contains(packet.type())) {
-                pair.getKey().onPacketReceived(client, packet);
+        this.packetHooks.put(PacketType.FAILURE, new PacketListener() {
+            @Override
+            public void onPacketReceived(Client client, Packet packet) {
+                FailurePacket fp = (FailurePacket)packet;
+                System.out.println("Failure Packet: [errId: " + fp.errorId + ", " + fp.errorMessage + "]");
             }
-        }        
-    }    
+        });
+    }
 }
