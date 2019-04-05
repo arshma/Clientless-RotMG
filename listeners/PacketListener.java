51ffb1d070e5fe58e4d9be32ce79de84c857e388
Arsh Mangat <>
2019-04-05 14:54:36 -0700

    Removed build files

--- listeners/PacketListener.java
+++
@@ -1,9 +0,0 @@
-package listeners;
-
-import net.Client;
-import net.Client;
-import net.packets.Packet;
-
-public interface PacketListener {
-    public void onPacketReceived(Client client, Packet packet);    
-}
